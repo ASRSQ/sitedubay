e0a6b153e368a58f2b05df4a1034b47788f08d1c
ASRSQ <a.s.rwar@gmail.com>
2025-01-14 19:21:03 -0300

    Teste imagem commit do projeto sitedubay

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -95,7 +95,7 @@
     padding: 5px 15px; /* Espaçamento interno do retângulo */
     color: #000;
     font-weight: bold;
-    box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
+    
     width: 87%; /* Ajusta para caber na coluna */
     height: 50px; /* Define uma altura fixa */
     position: relative; /* Permite posicionamento do círculo */
